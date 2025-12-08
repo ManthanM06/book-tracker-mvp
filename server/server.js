7c8ecaae893d6b635a2a8da28433b2aa5de1b2fd
manthan <manthan12333@gmail.com>
2025-12-08 10:53:56 +0530

    Initial commit: Project structure and backend setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors()); //allows frontend to make requests
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Serever running on port ${PORT}`);
+});
